7b973bb72dea90a6228f5052683e9988e7619ced
diddnjswns <diddnjswns20@gmail.com>
2023-04-19 19:58:03 +0900

    [feat] 백테스팅 post 요청 폼

--- front/src/components/Pages/BackTesting.js
+++ front/src/components/Pages/BackTesting.js
@@ -1,21 +1,53 @@
-import React from 'react';
-import { Grid, Box } from '@mui/material';
+import React, { useState } from "react";
+import axios from "axios";
 
 const BackTesting = () => {
+
+        const [coinName, setCoinName] = useState("");
+        const [parameter, setParameter] = useState("");
+        const [term, setTerm] = useState("");
+        const [testSize, setTestSize] = useState("");
+        const [imgPath, setImgPath] = useState("");
+        const [result, setResult] = useState("");
+
+        const handleSubmit = (e) => {
+            e.preventDefault(); //기본 폼 제출 방지
+        
+            const formData = { coin_name: coinName, parameter, term, test_size: testSize, ImgPath: imgPath };
+        
+            axios.post("/api/start_bot", formData) //백엔드에 post 요청
+            .then(res => setResult(JSON.stringify(res.data))) //응답 받으면 result에 저장
+            .catch(err => console.log(err));
+        };
+
     return(
         <>
-        <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={1}>
-            <Grid item xs={12} sm={6}>
-                오른
-            {/* 왼쪽 영역 */}
-            </Grid>
-            <Grid item xs={12} sm={6}>
-            {/* 오른쪽 영역 */}
-            왼
-            </Grid>
-        </Grid>
-        </Box>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="coinName">Coin Name:</label>
+                <input type="text" id="coinName" value={coinName} onChange={(e) => setCoinName(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="parameter">Parameter:</label>
+                <textarea id="parameter" value={parameter} onChange={(e) => setParameter(e.target.value)}></textarea>
+            </div>
+            <div>
+                <label htmlFor="term">Term:</label>
+                <input type="number" id="term" value={term} onChange={(e) => setTerm(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="testSize">Test Size:</label>
+                <input type="number" id="testSize" value={testSize} onChange={(e) => setTestSize(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="imgPath">Image Path:</label>
+                <input type="text" id="imgPath" value={imgPath} onChange={(e) => setImgPath(e.target.value)} />
+            </div>
+            <button type="submit">Start Bot</button>  
+            <div> 
+                <pre>{result}</pre>
+            </div>
+        </form>
         </>
     )
 };
